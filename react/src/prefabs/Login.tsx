a4ee354aa0568c8332ca66cfd9a7af10c6f2be5f
Kasper Kilde <kasperkilde2@gmail.com>
2024-06-07 11:41:48 +0200

    chore: Update API endpoints to use port 3000 for backend server

--- react/src/prefabs/Login.tsx
+++ react/src/prefabs/Login.tsx
@@ -13,7 +13,7 @@ const Login: React.FC<LoginProps> = ({ onLogin }) => {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch("http://10.0.0.155/login", {
+      const response = await fetch("http://10.0.0.155:3000/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
@@ -35,7 +35,7 @@ const Login: React.FC<LoginProps> = ({ onLogin }) => {
 
   const handleRegister = async () => {
     try {
-      const response = await fetch("http://10.0.0.155/register", {
+      const response = await fetch("http://10.0.0.155:3000/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password, phone }),
